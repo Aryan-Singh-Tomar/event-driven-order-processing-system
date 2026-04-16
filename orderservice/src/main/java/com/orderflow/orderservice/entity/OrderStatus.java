d2bf12fd6e42f2c49ee61ada93de574fb773398c
Aryan-tech29 <aaryan29.singh@gmail.com>
2026-04-16 16:49:23 +0530

    feat(entity): add Order entity, OrderStatus enum, and OrderRepository with PostgreSQL config

---
+++ orderservice/src/main/java/com/orderflow/orderservice/entity/OrderStatus.java
@@ -0,0 +1,9 @@
+package com.orderflow.orderservice.entity;
+
+public enum OrderStatus {
+    CREATED,
+    CONFIRMED,
+    PROCESSING,
+    SHIPPED,
+    DELIVERED
+}
---
+++ orderservice/src/main/java/com/orderflow/orderservice/repository/OrderRepository.java
@@ -0,0 +1,9 @@
+package com.orderflow.orderservice.repository;
+
+import com.orderflow.orderservice.entity.Order;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface OrderRepository extends JpaRepository<Order, Long> {
+}
